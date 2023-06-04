0c5b07426f88e7c86e5c163f171c38471191b1e5
Anton K <akarpenko83@gmail.com>
2023-06-04 17:01:38 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,41 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-import { Header, Section, Container, Text } from 'components';
+import {
+    Header,
+    Section,
+    Container,
+    Text,
+} from 'components';
 import { Gallery, Todos } from 'tabs';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header />
 
-      <Section>
-        <Container>
-          <Tabs>
-            <TabList>
-              <Tab>
-                <Text>Gallery</Text>
-              </Tab>
-              <Tab>
+            <Section>
+                <Container>
+                    <Tabs>
+                        <TabList>
+                            <Tab>
+                                <Text>Gallery</Text>
+                            </Tab>
+                            {/* <Tab>
                 <Text>Todos</Text>
-              </Tab>
-            </TabList>
+              </Tab> */}
+                        </TabList>
 
-            <TabPanel>
-              <Gallery />
-            </TabPanel>
+                        <TabPanel>
+                            <Gallery />
+                        </TabPanel>
 
-            <TabPanel>
-              <Todos />
-            </TabPanel>
-          </Tabs>
-        </Container>
-      </Section>
-    </>
-  );
+                        <TabPanel>
+                            <Todos />
+                        </TabPanel>
+                    </Tabs>
+                </Container>
+            </Section>
+        </>
+    );
 };

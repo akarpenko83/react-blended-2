65e90acda66d628d5b5cb3cb1fdeb98154b5eefc
Anton K <akarpenko83@gmail.com>
2023-06-04 15:20:54 +0300

    update

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,39 @@
 import { Component } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
-import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+import {
+    FormBtn,
+    InputSearch,
+    SearchFormStyled,
+} from './SearchForm.styled';
 
 export class SearchForm extends Component {
-  render() {
-    return <h2>SearchForm</h2>;
-  }
+    state = {
+        value: '',
+    };
+    onInputChange = e => {
+        this.setState({ value: e.target.value });
+    };
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.value);
+        this.setState({ value: '' });
+    };
+    render() {
+        return (
+            <SearchFormStyled onSubmit={this.onSubmit}>
+                <FormBtn type="submit">
+                    <FiSearch size="16px" />
+                </FormBtn>
+                <InputSearch
+                    value={this.state.value}
+                    placeholder="What do you want to write?"
+                    name="search"
+                    required
+                    autoFocus
+                    onChange={this.onInputChange}
+                />
+            </SearchFormStyled>
+        );
+    }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,14 +1,31 @@
 import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
-import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import {
+    Button,
+    SearchForm,
+    Grid,
+    GridItem,
+    Text,
+    CardItem,
+} from 'components';
 
 export class Gallery extends Component {
-  render() {
-    return (
-      <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
-      </>
-    );
-  }
+    state = {
+        value: '',
+    };
+    handleSubmit = value => {
+        console.log('gallery', value);
+        this.setState({ value });
+    };
+    render() {
+        return (
+            <>
+                <SearchForm onSubmit={this.handleSubmit} />
+                <Text textAlign="center">
+                    Sorry. There are no images ... 😭
+                </Text>
+            </>
+        );
+    }
 }
